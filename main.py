f9aafb33ba39d3355936929660d8dc541bef1b82
Brucktings <fabianadsuar@gmail.com>
2025-05-15 14:00:52 +0200

    draw spritres

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    pygame.time.Clock()
+    dt = 0
     running = True
     while running:
         for event in pygame.event.get():
@@ -20,8 +21,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
-    pygame.quit()
+    pygame.time.Clock.tick(60)
+    dt = pygame.time.get_ticks / 1000
+pygame.quit()
 
 if __name__ == "__main__":
     main()
