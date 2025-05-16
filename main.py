b1c0f51912b5111324950596413419e8bec00006
brucktings <fabianadsuar@gmail.com>
2025-05-16 08:15:09 +0200

    added groups to container

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
 
@@ -22,9 +24,9 @@ def main():
                 running = False
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        
+        updatable.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
